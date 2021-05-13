abc0338fa2ef52df7430f1cc419863e924da6015
Luca Borzacchiello <lucaborza@gmail.com>
2021-05-13 12:18:16 +0200

    AngrFast: fixed typo

--- cfg_extractors/angr_plugin/fast.py
+++ cfg_extractors/angr_plugin/fast.py
@@ -2,8 +2,8 @@ from cfg_extractors.angr_plugin.common import AngrCfgExtractor
 
 class AngrCfgExtractorFast(AngrCfgExtractor):
     def _get_angr_cfg(self, proj, addr):
-        return proj.analyses.CFG(
+        return proj.analyses.CFGFast(
             normalize=False,
             use_patches=True,
-            starts=[addr],
+            function_starts=[addr],
             resolve_indirect_jumps=True)
