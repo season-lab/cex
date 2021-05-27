f3f20603cde7f8e5f14e4e7c474e802a93a078a9
Luca Borzacchiello <lucaborza@gmail.com>
2021-05-27 13:27:44 +0200

    circular import fix

--- cfg_extractors/__init__.py
+++ cfg_extractors/__init__.py
@@ -1,4 +1,4 @@
-from utils import normalize_graph
+import utils as u
 from yapsy.IPlugin import IPlugin
 
 import os
@@ -113,7 +113,7 @@ class ICfgExtractor(IPlugin):
 
     @staticmethod
     def normalize_graph(graph):
-        return normalize_graph(graph)
+        return u.normalize_graph(graph)
 
     def clear_cache(self):
         return  # Look in subclasses
